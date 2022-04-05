f076a63542e34aecb2054f1a89cfe96efce2d988
IbragimovaS <ibragimova.sholpan.e@gmail.com>
2022-04-05 17:53:16 +0300

    Update delete_cron.py

--- Parsers/crontab/delete_cron.py
+++ Parsers/crontab/delete_cron.py
@@ -1,8 +1,8 @@
 from crontab import CronTab
 
 
-my_cron = CronTab(user='sholpan')
+my_cron = CronTab(user='user')
 for job in my_cron:
-    if job.command == 'python3 /home/sholpan/PycharmProjects/crontab/instagram.py \'1626737751\' 01.12.19 01.09.19':
+    if job.command == 'python3 /home/user/PycharmProjects/crontab/instagram.py \'1626737751\' 01.12.19 01.09.19':
         my_cron.remove(job)
         my_cron.write()
