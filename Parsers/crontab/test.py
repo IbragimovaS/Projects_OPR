9be94ba9edc1798a585e4175c0906e57717346e4
IbragimovaS <ibragimova.sholpan.e@gmail.com>
2022-04-05 17:53:39 +0300

    Update test.py

--- Parsers/crontab/test.py
+++ Parsers/crontab/test.py
@@ -1,6 +1,6 @@
 import datetime
 
-with open("/home/sholpan/PycharmProjects/crontab/Output.txt", 'a') as the_file:
+with open("/home/user/PycharmProjects/crontab/Output.txt", 'a') as the_file:
     the_file.write("Write at: %s" %datetime.datetime.now() + '\n')
 
 the_file.close()
